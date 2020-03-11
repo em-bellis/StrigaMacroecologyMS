f379b7002cc096a70ba37ce2bd145948cc0f583c
Emily Bellis <emilysbellis@gmail.com>
2020-03-11 16:23:09 -0500

    Update future_ENMs.R

--- future_ENMs.R
+++ future_ENMs.R
@@ -5,6 +5,7 @@ library(raster)
 library(viridis)
 library(maps)
 library(pals)
+library(dismo)
 
 all <- raster('/Users/ebellis/Desktop/Projects/StigaxSorghum/MacroecologyMS/Fig1/all.CLY250.tif')
 maiz <- raster('/Users/ebellis/Desktop/Projects/StigaxSorghum/MacroecologyMS/Fig1/maiz.CLY250.tif')
@@ -12,6 +13,14 @@ sorg <- raster('/Users/ebellis/Desktop/Projects/StigaxSorghum/MacroecologyMS/Fig
 mill <- raster('/Users/ebellis/Desktop/Projects/StigaxSorghum/MacroecologyMS/Fig1/mill.CLY250.tif')
 
 mods.stack <- stack(all, maiz, mill,sorg)
+
+#for histogram of average change, create a mask based on 200 km radius of ALL known occurrences
+### replace outside of 200 km w/value of 0
+shgeo <- read.csv('/Users/ebellis/Desktop/Projects/StigaxSorghum/MacroecologyMS/Fig1/curated_occ_ESB_7.26.18.csv', header=T,stringsAsFactors=F)
+coordinates(shgeo) <- ~lon+lat
+projection(shgeo) <- CRS('+proj=longlat +datum=WGS84')
+x <- circles(shgeo, d=200000, lonlat=T) #sample within radius of 200km
+pol <- polygons(x)
 
 mods <- c('all','maiz','mill','sorg')
 rcps <- c('rcp45.','rcp85.')
@@ -31,15 +40,26 @@ for (k in 1:2) {
  		diff <- fut.mean - mods.stack[[j]]
  	
  	#mask based on minimum habitat suitability in the future
- 		core <- calc(fut.mean, fun=function(x){ x[x < 0.2] <- NA; return(x)} )
- 		diff.core <- mask(diff, core)
+ 		#core <- calc(fut.mean, fun=function(x){ x[x < 0.2] <- NA; return(x)} )
+ 		#diff.core <- mask(diff, core)
  	
- 		out.file <- paste("/Users/ebellis/Desktop/Projects/StigaxSorghum/ENMs/future/images/",rcps[k],mods[j],'.pdf', sep="")
+ 		#out.file <- paste("/Users/ebellis/Desktop/Projects/StigaxSorghum/ENMs/future/images/",rcps[k],mods[j],'.pdf', sep="")
 
- 		pdf(out.file)
- 		plot(diff.core, breaks=seq(-1,1,by=0.1),col=coolwarm(n=20), legend=T,  xaxt='n', yaxt='n')
- 		map(database="world", xlim=c(-20,60),ylim=c(-40,45),axes=FALSE, add=T, col="grey50", lwd=0.5)
- 		scalebar(1000, xy = c(35,-33), type = "line", divs = 1, lonlat = TRUE)
- 		dev.off()
+ 		#pdf(out.file)
+ 		#plot(diff.core, breaks=seq(-1,1,by=0.1),col=coolwarm(n=20), legend=T,  xaxt='n', yaxt='n')
+ 		#map(database="world", xlim=c(-20,60),ylim=c(-40,45),axes=FALSE, add=T, col="grey50", lwd=0.5)
+ 		#scalebar(1000, xy = c(35,-33), type = "line", divs = 1, lonlat = TRUE)
+ 		#dev.off()
+ 	
+ 		
+ 		##crop to polygon, convert to dataframe, and test whether true location shift is equal to 0
+ 		fut.within <- mask(fut.mean, pol)
+ 	   mod.within <- mask(mods.stack[[j]], pol)
+ 	   tmp <- stack(fut.within, mod.within)
+ 	   tmp.df <- as.data.frame(tmp, na.rm=TRUE)
+ 	   colnames(tmp.df) <- c("future","current")
+ 	   
+ 	   print(paste("For ",rcps[k]," and ", mods[j],"model:"), sep="")
+ 	   wilcox.test(tmp.df$future, tmp.df$current, paired = T, conf.int=T)
 	}
 }
