4dcb959f623ac5a4841a93a6e0268768bebfa91b
Emily Bellis <emilysbellis@gmail.com>
2020-03-31 09:50:57 -0500

    function for mean and sd within specified radius

---
+++ radius_function.R
@@ -0,0 +1,13 @@
+radius_stats <- function(enm, radius, location) {
+	#location given as SpatialPoints object
+	#enm is raster layer
+	#radius is integer (number of meters)
+	
+	x <- circles(location, d=radius, lonlat=T)
+	pol <- polygons(x)
+	masked <- mask(enm, pol)
+	mean.enm <- cellStats(masked, 'mean', na.rm=T)
+	sd.enm <- cellStats(masked, 'sd', na.rm=T)
+	my_list <- list("mean" = mean.enm, "sd" = sd.enm)
+	return(my_list)
+}
