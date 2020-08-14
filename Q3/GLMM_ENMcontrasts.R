5cc95f1b42b0d578ef904b60eeac4b201088c412
chloee-mclaughlin <56924434+chloee-mclaughlin@users.noreply.github.com>
2020-08-14 15:07:21 -0400

    Update GLMM_ENMcontrasts.R
    Update relative path to new data folder, removed Std. Dev. data

--- Q3/GLMM_ENMcontrasts.R
+++ Q3/GLMM_ENMcontrasts.R
@@ -4,7 +4,7 @@
 library(lme4)
 library(readr)
 
-#setwd("~/Desktop/Research/Lasky/StrigaMacroecology/ModelWD/")
+#setwd("~/StrigaMacroecologyMS/DataFiles/")
 
 ##Making data table only including locaitons with comparative host species
 New.Stand <- read.csv("New.Stand.1.11.20.csv")
@@ -18,6 +18,7 @@ Emerg.dat <- data.frame(emergence=New.Stand$emergence, host=New.Stand$host,
 SMZ.dat <- data.frame(locality= ENM.all$locality,lat= ENM.all$lat, lon= ENM.all$lon, ENM_a_s50km=ENM.all$ENM_a_s50km, 
                       ENM_a_m50km=ENM.all$ENM_a_m50km, ENM_a_z50km=ENM.all$ENM_a_z50km)
 
+##combining emergence from empircal studies with constructed ENMs
 SI.dat <- merge(Emerg.dat, SMZ.dat, by="locality", all=TRUE)
 
 ##Sorghum model
@@ -29,6 +30,7 @@ anova(s.1.enm, s.2.enm, test="Chisqu")
 ##Millet model
 m.1.enm <-lmer(emergence ~ (1 | host.gen), data=SI.dat[SI.dat$host=="millet",])
 m.2.enm <-lmer(emergence ~ (1 | host.gen) + ENM_a_m50km, data=SI.dat[SI.dat$host=="millet",])
+
 anova(m.1.enm, m.2.enm, test="Chisqu")
 
 ##Maize model
@@ -46,61 +48,9 @@ anova(z.1.enm, z.2.Senm, test=("Chisq"))
 z.2.Menm <- lmer(emergence ~ (1 | host.gen) + ENM_a_m50km, data=SI.dat[SI.dat$host=="maize",])
 anova(z.1.enm, z.2.Menm, test=("Chisq"))
 
-###################################
-##ENM Standard Deviation
-##new dataframe with Standard Deviations of ENM for locations, already has all other data columns
-SD <- read_csv("SI.dat.4.2.20_StDev.csv")
-
-##Sorghum
-##Fixed effect of SD
-s.ENM.SD.f <-lmer(emergence ~ (1 | host.gen) + ENM_a_s50km + ENM_avs_sd, data=SD[SD$host=="sorghum",])
-##same models as previous section, had to be recreated because with a new dataset
-s <-lmer(emergence ~ (1 | host.gen),  data=SD[SD$host=="sorghum",])
-s.ENM <-lmer(emergence ~ (1 | host.gen) + ENM_a_s50km,  data=SD[SD$host=="sorghum",])
-
-##NO SD, confirm same output
-anova(s, s.ENM, test=("Chisq"))
-##anova comparing emergence vs emergence with ENM and SD as fixed effects
-anova(s.ENM.SD.f, s, test=("Chisq"))
-
-
-##Millet
-m.ENM.SD.f <-lmer(emergence ~ (1 | host.gen) + ENM_a_m50km + ENM_avm_sd, data=SD[SD$host=="millet",])
-m.ENM <-lmer(emergence ~ (1 | host.gen) + ENM_a_m50km,  data=SD[SD$host=="millet",])
-m <-lmer(emergence ~ (1 | host.gen),  data=SD[SD$host=="millet",])
-###NO SD 
-anova(m, m.ENM, test=("Chisq"))
-###including SD and ENM as fixed 
-anova(m, m.ENM.SD.f, test=("Chisq"))
-
-##Maize
-z.ENM.SD.f <-lmer(emergence ~ (1 | host.gen) + ENM_a_z50km + ENM_avz_sd, data=SD[SD$host=="maize",])
-z.ENM <-lmer(emergence ~ (1 | host.gen) + ENM_a_z50km,  data=SD[SD$host=="maize",])
-z <-lmer(emergence ~ (1 | host.gen),  data=SD[SD$host=="maize",])
-##NO SD
-anova(z, z.ENM, test=("Chisq"))
-##Including ENM and SD
-anova(z, z.ENM.SD.f, test=("Chisq"))
-
-##Map of the locations used from empirical studies
-library(rworldmap)
-newmap <- getMap(resolution = "low")
-plot(newmap)
-plot(newmap,
-     xlim = c(-20, 59),
-     ylim = c(-15, 40),
-     asp = 1 )
-plot(newmap,
-     xlim = range(SI.dat$lat),
-     ylim = range(SI.dat$lon),
-     asp = 1
-)
-points(SI.dat$lat, SI.dat$lon, col = "red", cex = .6)
-
-##########################################
 ##approximated coefficents of linear models using Satterwaithes method for crop harvest
 ##note:: If the error code "[,5] out of bounds"appears, re-run models with "lmerTest" which 
-#includes approximates more model parameters (including p-values) than "lme4"
+#includes more columns (approximate model parameters e.g. p-values) than "lme4"
 
 require(lmerTest)
 
@@ -129,3 +79,19 @@ coef.z$p.z <- 2 * (1 - pnorm(abs(coefs$t.value)))
 coef.z$df.Satt <- coef(summary(z.2.enm))[, 3]
 coef.z$p.Satt <- coef(summary(z.2.enm))[, 5]
 coef.z
+
+#####
+##Map of the locations used from empirical studies
+library(rworldmap)
+newmap <- getMap(resolution = "low")
+plot(newmap)
+plot(newmap,
+     xlim = c(-20, 59),
+     ylim = c(-15, 40),
+     asp = 1 )
+plot(newmap,
+     xlim = range(SI.dat$lat),
+     ylim = range(SI.dat$lon),
+     asp = 1
+)
+points(SI.dat$lat, SI.dat$lon, col = "red", cex = .6)
